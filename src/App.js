67a96d7fdc5683121b6af97080e00cddc76083d5
Tarek Siddique Nabil <nabilh420@gmail.com>
2022-11-08 03:54:04 +0600

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Category from './Component/Chart/Category/Category';
+import Monthly from './Component/Chart/Monthly/Monthly';
+import Total from './Component/Chart/Total/Total';
 import Nav from "./Component/Navbar/Nav";
 
 
 
 function App() {
   return (
-    <div>
-      <Nav></Nav>
-      {/* <Home></Home> */}
-      {/* <Total></Total> */}
-      </div>
+      <Router>
+      <Routes>
+      <Route path='/' element={<Nav/>} />
+      <Route path='/total' element={<Total/>} />
+      <Route path='/monthly' element={<Monthly/>} />
+      <Route path='/category' element={<Category/>} />
+      </Routes>
+      </Router>
   );
 }
 
--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,4 +1,5 @@
 
+import { NavLink } from "react-router-dom";
 import gif from "../Img/Revenue.gif";
 import "./Home.css";
 
@@ -20,7 +21,7 @@ const Home=()=> {
       <p className="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">Never spend your money before you have earned it.</p>
       <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
         <div className="rounded-md shadow">
-          <a href="" className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg">Get started</a>
+          <a href="www.youtube.com" className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg">Get started</a>
         </div>
       </div>
     </div>
@@ -29,8 +30,38 @@ const Home=()=> {
 <div  className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 ">
 <img id="image" className=" w-full object-cover sm:h-72 md:h-96 lg:h-full lg:w-full lg:mt-12" src={gif} alt=""/>
 </div>
-</div>
-    
+  <br/>
+  <div id="dashboard">
+  <header class="bg-white shadow">
+    <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8 text-center">
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+    </div>
+  </header>
+   <div className="flex justify-center space-x-4 mt-3">
+   <NavLink to="/total">
+        <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300  shadow-lg shadow-blue-500/50  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Total</button>
+         </NavLink>
+
+         <NavLink to="/monthly">
+        <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300  shadow-lg shadow-blue-500/50  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 ">Monthly</button>
+         </NavLink>
+
+        <NavLink to="/category">
+        <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300  shadow-lg shadow-blue-500/50  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 ">Category</button>
+         </NavLink>
+        
+
+   </div>
+  <main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+      
+      <div class="px-4 py-6 sm:px-0">
+        <div class="h-96 rounded-lg border-4 border-dashed border-gray-200"></div>
+      </div>
+    </div>
+  </main>
+  </div>
+    </div>
   );
 };
 export default Home;
